2078583fb83aacaf03ae6e3781abef0a786e9f64
Sudheep <sudheepmuruganantham@gmail.com>
2025-12-02 11:51:07 +0530

    Fix dotenv loading and configuration

--- server.js
+++ server.js
@@ -56,5 +56,5 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
