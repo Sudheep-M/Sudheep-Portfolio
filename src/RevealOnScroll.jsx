5639e9f2b76cb7cdea3527a48ec3156d9387acc6
Sudheep <sudheepmuruganantham@gmail.com>
2026-02-24 17:15:30 +0530

    fixed revealOnScroll

--- src/RevealOnScroll.jsx
+++ src/RevealOnScroll.jsx
@@ -32,10 +32,10 @@ const RevealOnScroll = ({
   }, [threshold, rootMargin, once]);
 
   const animations = {
-    "fade-up": "md:translate-x-80 translate-y-40 opacity-0",
-    "fade-down": "md:-translate-y-80 translate-x-40 opacity-0",
-    "fade-left": "md:-translate-x-80 -translate-x-40 opacity-0",
-    "fade-right": "md:-translate-x-80 translate-x-40 opacity-0",
+    "fade-up": "md:translate-x-80 translate-y-10 opacity-0",
+    "fade-down": "md:-translate-y-80 translate-x-10 opacity-0",
+    "fade-left": "md:-translate-x-80 -translate-x-10 opacity-0",
+    "fade-right": "md:-translate-x-80 translate-x-10 opacity-0",
     zoom: "scale-0 opacity-0",
   };
 
